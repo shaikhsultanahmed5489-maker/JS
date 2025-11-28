85f2cd990887dc72d5404aa65a70cd96a68c5110
shaikhsultanahmed5489-maker <shaikhsultanahmed5489@gmail.com>
2025-11-28 13:33:02 +0000

    a little js

--- 01_Basics/conversionOpertaor.js
+++ 01_Basics/conversionOpertaor.js
@@ -1,12 +1,21 @@
 let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33" => 33
 //"33abc" => NaN
 // true =>1; false => 0 
+
+
+//************** Operations *********************/
+
+str1 = "Sultan "
+str2 = "Ahmed"
+
+str3 = str1 + str2
+console.log(str3);
