d5dc21c9d7431afaab2c0f3eec99584a6d304299
shaikhsultanahmed5489-maker <shaikhsultanahmed5489@gmail.com>
2025-11-28 11:44:59 +0000

    conversion of operator

--- 01_Basics/conversionOpertaor.js
+++ 01_Basics/conversionOpertaor.js
@@ -5,4 +5,8 @@ console.log(typeof (score))
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
-console.log(valueInNumbe)
+console.log(valueInNumber)
+
+// "33" => 33
+//"33abc" => NaN
+// true =>1; false => 0 
