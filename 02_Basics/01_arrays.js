487f8c7e8d6e2f0e0d8f5707617dbd8a682e0a1e
shaikhsultanahmed5489-maker <shaikhsultanahmed5489@gmail.com>
2025-11-30 18:16:38 +0000

    arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,41 @@
+const arr = [0,1,2,3,4]
+const myheros = ["superman", "Batman"]
+
+const arr2 = new Array(1, 2, 3, 4)
+// console.log(arr[0]);
+
+
+// Array Methods 
+
+// arr.push(5)
+// arr.pop()
+
+// arr.unshift(3)
+// arr.shift()
+
+// console.log(arr.includes(7));
+// console.log(arr.indexOf(3));
+
+
+// const newarr = arr.join()
+
+// console.log(newarr);
+// console.log(typeof newarr);
+
+// console.log(arr);
+
+
+// Slice , Splice
+
+console.log("A", arr);
+
+const myn1 = arr.slice(1,3)
+
+console.log(myn1);
+console.log("B", arr);
+
+const myn2 = arr.splice(1,3)
+console.log("C", arr);
+console.log(myn2);
+
+
