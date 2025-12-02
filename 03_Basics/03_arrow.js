e68e883256026881daee4c2f24da799a196762f2
shaikhsultanahmed5489-maker <shaikhsultanahmed5489@gmail.com>
2025-12-02 05:19:09 +0000

    scope & arrow

---
+++ 03_Basics/03_arrow.js
@@ -0,0 +1,55 @@
+const user = {
+    username: "Sultan",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "Sultan"
+//     console.log(this.username);
+// }
+
+// chai()
+
+
+// const chai = function() {
+//     let username = "Sultan"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+
+
+const chai =  () => {
+    let username = "Sultan"
+    console.log(this);
+}
+
+
+// chai()
+
+
+// const addtwo = (num1 , num2) => {
+//     return num1 + num2
+// }
+
+
+// const addtwo = (num1 , num2) => num1+num2
+
+// const addtwo = (num1 , num2) => (num1+num2)
+
+const addtwo = (num1 , num2) => ({username: "Sultan"})
+
+console.log(addtwo(3,10));
