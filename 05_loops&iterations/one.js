2fe54129165876a82ad3c2f3e367787dad9fba16
shaikhsultanahmed5489-maker <shaikhsultanahmed5489@gmail.com>
2025-12-06 15:24:20 +0000

    loops

---
+++ 05_loops&iterations/one.js
@@ -0,0 +1,56 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+    
+}
+
+// console.log(element);
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Outer loop value${i}`);
+    
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`inner loop value ${j} and ${i}`);
+        // console.log(i + '*' + j + ' = ' + i*j );
+       
+    }    
+}
+
+
+let myarray = ["flash", "batman", "superman"]
+
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    // console.log(element);
+    
+    
+}
+
+
+// break & Continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`Value of i is ${index}`);
+    
+    
+// }
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`Value of i is ${index}`);
+    
+    
+}
---
+++ 05_loops&iterations/two.js
@@ -0,0 +1,22 @@
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myarray = ["Flash", "Batman", "superman"]
+
+// let arr = 0
+// while (arr <= myarray.length) {
+//     console.log(`Value if ${myarray[arr]}`);
+//     arr = arr + 1
+    
+// }
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++    
+} while (score <= 10);
