53bb8e4ff5c189125962b8154dffa7ade2313c1e
shaikhsultanahmed5489-maker <shaikhsultanahmed5489@gmail.com>
2025-12-28 19:26:01 +0000

    iterations

--- 05_loops&iterations/one.js
+++ 05_loops&iterations/one.js
@@ -47,10 +47,10 @@ for (let index = 0; index < myarray.length; index++) {
 
 for (let index = 1; index <= 20; index++) {
     if (index == 5) {
-        console.log(`Detected 5`);
+        // console.log(`Detected 5`);
         continue
     }
-    console.log(`Value of i is ${index}`);
+    // console.log(`Value of i is ${index}`);
     
     
 }
