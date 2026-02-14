a0d8e0522a5685b35773d302d30ae84c79301d94
shaikhsultanahmed5489-maker <shaikhsultanahmed5489@gmail.com>
2026-02-14 19:03:14 +0000

    prototypes

--- 09_Advance/prototype.js
+++ 09_Advance/prototype.js
@@ -27,6 +27,45 @@ Array.prototype.heysultan = function() {
 
 
 // heroPower.sultan()
-myHeros.sultan()
-myHeros.heysultan()
+// myHeros.sultan()
+// myHeros.heysultan()
 // heroPower.heysultan()
+
+
+//inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignmnet',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__= User
+
+//modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+
+let anotherUsername = "ChaiAurCode               "
+
+String.prototype.trueLength = function() {
+    console.log(`${this}`);
+    console.log(`${this.name}`);
+    console.log(`True Length is: ${this.trim().length}`);
+    
+}
+
+anotherUsername.trueLength()
